00d0b9bce3de9b6752a407d53cb765a0259ff9f6
Karl Tiirik <karltiirik@gmail.com>
2014-11-13 21:14:56 +0200

    * Refactored ID generation (as a generator) * Refactored reading names' file * Refactored module11 * Replaced strftime with own code * Reorganized files in project
    From ~37 seconds, 268 MB of RAM to ~27 seconds and 5.5 MB of RAM

--- CSV_file_creator.py
+++ CSV_file_creator.py
@@ -1,38 +1,42 @@
 __author__ = 'Karl Tiirik'
-import est_id_gen
+
 import csv
+import random
+
+from est_id import est_id_gen
 
 
 def generate_csv_with_n_users(n):
-    """Generates a CSV file with n test persons' data"""
-    list_of_names = read_names("names.txt")
-    list_of_est_ids = est_id_gen.gen_n_persons(n)
+    """ Generates a CSV file with n test persons' data """
+    surnames, forenames = read_names('IO files\\names.txt')
 
-    with open('test_persons_data.csv', 'w', encoding="utf8") as test_persons:
+    with open('IO files\\test_persons_data.csv', 'w', encoding="utf8") as test_persons:
         wr = csv.writer(test_persons, delimiter=',', lineterminator='\n')
-        wr.writerow(["SOCIAL_SECURITY_NUMBER", "FORENAME", "SURNAME", "GENDER", "DATE_OF_BIRTH"])
-        for i in range(n):
-            forename = list_of_names[i % len(list_of_names)][1]
-            surname = list_of_names[i % len(list_of_names)][0]
-            persons_data = [list_of_est_ids[i].code, forename, surname,
-                            list_of_est_ids[i].gender, list_of_est_ids[i].birth_date.strftime('%d.%m.%Y')]
+        wr.writerow(['SOCIAL_SECURITY_NUMBER', "FORENAME", "SURNAME", "GENDER", "DATE_OF_BIRTH"])
+
+        for id_code in est_id_gen.id_generator(n):
+            b_date = str(id_code.birth_date.day) + str(id_code.birth_date.month) + str(id_code.birth_date.year)[2:]
+            persons_data = [id_code.code, random.choice(forenames), random.choice(surnames), id_code.gender, b_date]
             wr.writerow(persons_data)
 
 
 def read_names(file_name):
-    """ Read files from csv format, save as list. Removes problematic lines"""
-    with open(file_name, "r", encoding="utf8") as names_file:
+    """ Read files from csv format, save as list. Removes problematic lines """
+    with open(file_name, 'r', encoding='utf8') as names_file:
         raw_list_of_names = names_file.read().splitlines()
 
-    list_of_names = []
+    surnames, forenames = [], []
     for i in range(len(raw_list_of_names)):
         try:
             surname, forename = raw_list_of_names[i].split(", ")
-            list_of_names.append([surname, forename])
+            surnames.append(surname)
+            forenames.append(forename)
         except ValueError:  # If there is only 1 name
-            pass
-    return list_of_names
+            forenames.append(raw_list_of_names[i])
+    return surnames, forenames
+
 
 if __name__ == "__main__":
     import cProfile
-    cProfile.run('generate_csv_with_n_users(1000000)', sort='tottime')  # ~37 seconds
+
+    cProfile.run('generate_csv_with_n_users(1000000)', sort='tottime')  # ~27 seconds, 268 MB of RAM
